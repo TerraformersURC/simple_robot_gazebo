ca627088bca3dfb89397a4bebedeb3e8571c3656
Trevor Smith <8tsmith11@gmail.com>
2025-02-27 20:05:24 -0500

    Renamed file references to be accurate

--- launch/start_world.launch.py
+++ launch/start_world.launch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -6,17 +7,20 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_prefix
-from launch_ros.actions import Node
 
 def generate_launch_description():
 
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    pkg_simple_robot_gazebo = get_package_share_directory('simple_robot_gazebo')
+    pkg_box_bot_gazebo = get_package_share_directory('simple_robot_gazebo')
 
+    # We get the whole install dir
+    # We do this to avoid having to copy or softlink manually the packages so that gazebo can find them
     description_package_name = "simple_robot_description"
     install_dir = get_package_prefix(description_package_name)
 
-    gazebo_models_path = os.path.join(pkg_simple_robot_gazebo, 'models')
+    # Set the path to the WORLD model files. Is to find the models inside the models folder in my_box_bot_gazebo package
+    gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
+    # os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
 
     if 'GAZEBO_MODEL_PATH' in os.environ:
         os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
@@ -28,6 +32,8 @@ def generate_launch_description():
     else:
         os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
 
+    
+
     print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
     print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
 
@@ -38,21 +44,10 @@ def generate_launch_description():
         )
     )    
 
-    spawn_robot = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-entity', 'my_robot',
-            '-topic', 'robot_description'
-        ],
-        output='screen'
-    )
-
     return LaunchDescription([
         DeclareLaunchArgument(
           'world',
-          default_value=[os.path.join(pkg_simple_robot_gazebo, 'worlds', 'test_world.world'), ''],
+          default_value=[os.path.join(pkg_box_bot_gazebo, 'worlds', 'box_bot_empty.world'), ''],
           description='SDF world file'),
         gazebo
     ])
-
